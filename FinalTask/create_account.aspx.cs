d0541a15f049cdfa7efdbf4fe8bf5aff9ac043f1
Alexkarachalios <alexkarach5@gmail.com>
2019-06-25 22:34:41 +0300

    ok

--- FinalTask/create_account.aspx.cs
+++ FinalTask/create_account.aspx.cs
@@ -59,13 +59,26 @@ namespace FinalTask
 
         protected void create_button_Click(object sender, EventArgs e)
         {
+            var cmd1 = new NpgsqlCommand("SELECT username FROM users WHERE username='"+username_text.Text.ToString()+"'",conn);   
+            var result=cmd1.ExecuteScalar();
 
-            var cmd = new NpgsqlCommand("INSERT INTO users(name,surname,username,password) VALUES('"+first_text.Text.ToString()+"','"+last_text.Text.ToString()+"','"+username_text.Text.ToString()+"','"+password_text.Text.ToString()+"')",conn);
-           
-            //cmd.Prepare();
-            // Set parameters
-            cmd.ExecuteNonQuery();
-            // And so on
+            if (result != null)
+            {
+                string msg = "Username already exists!!!";
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + msg + "');", true);
+                
+            }
+            else
+            {
+                var cmd = new NpgsqlCommand("INSERT INTO users(name,surname,username,password) VALUES('" + first_text.Text.ToString() + "','" + last_text.Text.ToString() + "','" + username_text.Text.ToString() + "','" + password_text.Text.ToString() + "')", conn);
+
+                //cmd.Prepare();
+                // Set parameters
+                cmd.ExecuteNonQuery();
+                // And so on
+            }
+
+
         }
     }
 }
