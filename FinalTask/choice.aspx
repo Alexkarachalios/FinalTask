1cf6ec526ee47947c9673778b501fbf7cd26e7e5
Alexkarachalios <alexkarach5@gmail.com>
2019-06-26 02:35:27 +0300

    ok

---
+++ FinalTask/choice.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="choice.aspx.cs" Inherits="FinalTask.choice" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+         <div style ="background-color:#077390; width:100%; height:71px">
+        <h4 align="center" style="font-size:300%; height: 69px;">RENT-A-CAR </h4>
+    </div>
+         <p>
+             &nbsp;</p>
+         <p>
+             &nbsp;</p>
+         <p align="center">
+             <asp:Button  ID="Search" runat="server" OnClick="Button1_Click" style="margin-left: 0px" Text="Search" Width="202px" Height="48px" />
+         </p>
+         <p>
+             &nbsp;</p>
+         <p align="center">
+             <asp:Button  ID="NewEntry" runat="server" OnClick="Button1_Click" style="margin-left: 0px" Text="New Entry" Width="202px" Height="48px" />
+         </p>
+    </form>
+</body>
+</html>
--- FinalTask/login.aspx.cs
+++ FinalTask/login.aspx.cs
@@ -4,14 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Npgsql;
 
 namespace FinalTask
 {
     public partial class login : System.Web.UI.Page
     {
+        string localhost = "127.0.0.1";
+        string port = "5432";
+        string user = "postgres";
+        string pass = "1021989Alex";
+        string database = "postgres";
+        NpgsqlConnection conn;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            try
+            {
+                string connstring = String.Format("Server={0};Port={1};" +
+                   "User Id={2};Password={3};Database={4};",
+                   localhost, port, user,
+                   pass, database);
+                // Making connection with Npgsql provider
+                conn = new NpgsqlConnection(connstring);
+                conn.Open();
+            }
+            catch (Exception msg)
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + msg + "');", true);
+                throw;
+            }
         }
 
         protected void username_text_TextChanged(object sender, EventArgs e)
@@ -26,7 +48,19 @@ namespace FinalTask
 
         protected void login_button_Click(object sender, EventArgs e)
         {
+            var cmd1 = new NpgsqlCommand("SELECT username,password FROM users WHERE username='" + username_text.Text.ToString() + "' AND password='" + password_text.Text.ToString() + "'", conn);
+            var result = cmd1.ExecuteScalar();
 
+            if (result != null)
+            {
+                Response.Redirect("choice.aspx");
+
+            }
+            else
+            {
+                string msg = "Wrong username or password!!";
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + msg + "');", true);
+            }
         }
 
         protected void create_account_button_Click(object sender, EventArgs e)
