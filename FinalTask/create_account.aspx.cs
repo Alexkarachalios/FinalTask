760a044dd648fc4aa842b7a244e1e80c2510d02e
Alexkarachalios <alexkarach5@gmail.com>
2019-06-25 22:19:15 +0300

    Later bitches

--- FinalTask/create_account.aspx.cs
+++ FinalTask/create_account.aspx.cs
@@ -1,17 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Npgsql;
+using NpgsqlTypes;
 
 namespace FinalTask
 {
     public partial class create_account : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        string localhost="127.0.0.1";
+        string port = "5432";
+        string user = "postgres";
+        string pass = "1021989Alex";
+        string database = "postgres";
+        NpgsqlConnection conn;
+    protected void Page_Load(object sender, EventArgs e)
         {
-
+            try
+            {
+                string connstring = String.Format("Server={0};Port={1};" +
+                   "User Id={2};Password={3};Database={4};",
+                   localhost, port, user,
+                   pass, database);
+                // Making connection with Npgsql provider
+                conn = new NpgsqlConnection(connstring);
+                conn.Open();
+            }
+            catch (Exception msg)
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + msg + "');", true);
+                throw;
+            }
         }
 
         protected void first_text_TextChanged(object sender, EventArgs e)
@@ -37,6 +60,12 @@ namespace FinalTask
         protected void create_button_Click(object sender, EventArgs e)
         {
 
+            var cmd = new NpgsqlCommand("INSERT INTO users(name,surname,username,password) VALUES('"+first_text.Text.ToString()+"','"+last_text.Text.ToString()+"','"+username_text.Text.ToString()+"','"+password_text.Text.ToString()+"')",conn);
+           
+            //cmd.Prepare();
+            // Set parameters
+            cmd.ExecuteNonQuery();
+            // And so on
         }
     }
 }
