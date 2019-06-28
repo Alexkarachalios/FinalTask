4acc317ad7dc8eab2a639c045433b563f450006b
kompostas13 <kwnstadinosbb@hotmail.com>
2019-06-28 19:01:13 +0300

    εφτιαξα τη συνδεση με τη βαση για τα αμαξια...προσθεστε ενα column photo τυπου text για να εχουμε και την φωτο του αμαξιου....στην details εχω κανει τη συνδεση με τη βαση...πρεπει να βαλουμε στα label τα στοιχεια του αυτοκινητου απο τον χρηστη που διαλεξε στο search και να εμφανιζουμε τη φωτο του αυτοκινητου

---
+++ FinalTask/Details.aspx.cs
@@ -0,0 +1,42 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinalTask
+{
+    public partial class Details : System.Web.UI.Page
+    {
+        string localhost = "127.0.0.1";
+        string port = "5432";
+        string user = "postgres";
+        string pass = "13898301153KSXK"; //13898301153KSXK
+        string database = "postgres";
+        NpgsqlConnection conn;
+        NpgsqlConnection conn2;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string connstring = String.Format("Server={0};Port={1};" +
+                   "User Id={2};Password={3};Database={4};",
+                   localhost, port, user,
+                   pass, database);
+                // Making connection with Npgsql provider
+                conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                conn2 = new NpgsqlConnection(connstring);
+                conn2.Open();
+            }
+            catch (Exception msg)
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + msg + "');", true);
+                throw;
+            }
+
+        }
+    }
+}
--- FinalTask/new_entry.aspx.cs
+++ FinalTask/new_entry.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,13 +10,36 @@ namespace FinalTask
 {
     public partial class new_entry : System.Web.UI.Page
     {
+
+        string localhost = "127.0.0.1";
+        string port = "5432";
+        string user = "postgres";
+        string pass = "13898301153KSXK";//
+        string database = "postgres";
+        NpgsqlConnection conn;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            try
+            {
+                string connstring = String.Format("Server={0};Port={1};" +
+                   "User Id={2};Password={3};Database={4};",
+                   localhost, port, user,
+                   pass, database);
+                // Making connection with Npgsql provider
+                conn = new NpgsqlConnection(connstring);
+                conn.Open();
+            }
+            catch (Exception msg)
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + msg + "');", true);
+                throw;
+            }
         }
 
         protected void add_button_Click(object sender, EventArgs e)
         {
+            string usr = "'" + Session["username"].ToString() + "'";
             string s = FileUpload1.FileName;
             if (model_text.Text=="" | cc_text.Text=="" | km_text.Text=="" | year_text.Text=="" | price_text.Text=="" | s=="")
             {
@@ -24,7 +48,16 @@ namespace FinalTask
             }
             else
             {
-
+                
+                var cmd = new NpgsqlCommand("INSERT INTO cars(carowner,model,cc,price,km,year,photo) VALUES('" + Session["username"].ToString() + "','" + model_text.Text.ToString() + "','" + cc_text.Text.ToString() + "','" + price_text.Text.ToString() + "','" + km_text.Text.ToString() + "','" + year_text.Text.ToString() + "','" + s + "')", conn);
+                var cmd2 = new NpgsqlCommand("UPDATE USERS SET availability=true where username=" +  usr, conn);
+                //cmd.Prepare();
+                // Set parameters
+                cmd2.ExecuteNonQuery();
+                cmd.ExecuteNonQuery();
+                Response.Redirect("main.aspx");
+                // And so on
+           
             }
         }
     }
